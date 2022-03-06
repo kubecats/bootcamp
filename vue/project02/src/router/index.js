7a9b3d95cbad0b8122d315770432f2ab45da1dee
kubecats <76464384+kubecats@users.noreply.github.com>
2022-03-06 18:12:58 +0900

    vue

---
+++ vue/project02/src/router/index.js
@@ -0,0 +1,62 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+// import AboutView from '../views/AboutView.vue'
+// import HelloView from '../views/HelloView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/hello',
+    name: 'hello',
+    // component: HelloView
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "hello" */ '../views/HelloView.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // component: AboutView
+    component: () =>
+      import(
+        /* webpackChunkName: "about", webpackPrefetch:true */ '../views/AboutView.vue'
+      )
+  },
+  {
+    path: '/databinding/string',
+    name: 'DataBindingStringView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingStringView.vue'
+      )
+  },
+  {
+    path: '/databinding/html',
+    name: 'DataBindingHtmlView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingHtmlView.vue'
+      )
+  },
+  {
+    path: '/databinding/input',
+    name: 'DataBindingInputView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingInputView.vue'
+      )
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ vue/project02/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello, {{ userName }}</h1>
+    <h1>Welcom to, {{ userName2 }}</h1>
+    <p>{{ userName }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userName: 'hyunseung lee',
+      userName2: 'sktelecom'
+    }
+  }
+}
+</script>
+<style scoped></style>
